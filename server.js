a125e48839c921a6ef9241338bb4377e1716a455
ale <delacorte19@gmail.com>
2024-11-21 11:51:54 +0100

    se modifica y arregla el mongo

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
+const { error } = require("console");
 const express = require("express")
 const mongoose = require("mongoose")
 require('dotenv').config();
 const app = express()
-const port = 3000
+const port = 8000
+app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/jogos", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://127.0.0.1:27017/mongotest", {useNewUrlParser: true, useUnifiedTopology: true})
 
-const juegoSchema = new mongoose.Schema({
-    titulo: String,
-    desarrolladora: String,
-    "fecha-lanzamiento": String,
-    plataformas: Array
+const UsuarioSchema = new mongoose.Schema({
+    username: String,
+    password: String
+    
 })
 
-const Juego = mongoose.model('Juego', juegoSchema)
+const Usuarios = mongoose.model('users', UsuarioSchema);
 
-app.get("/", async (req,res) => {
-    const juegos = await Juego.find()
-    res.send(juegos)
+
+app.get("/", async (req, res) =>{
+    res.status(201).send("Holas")
+})
+
+app.get("/usuarios", async (req,res) => {
+        const users = await Usuarios.find()
+        res.json(users)
 })
 
 app.listen(port, () => {
